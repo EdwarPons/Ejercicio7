53bb1c1baf471ec1b7db22c832e9879ccea87518
Edwar Frank Pons Bessio <edwar.servidor@gmail.com>
2022-08-19 01:59:16 -0300

    Ejercicio 7 del Open Bootcamp

---
+++ index.js
@@ -0,0 +1,9 @@
+const myFamily = new Set(["Yesica", "Nelson", "Edwar", "María", "John"]);
+
+console.log(myFamily);
+
+myFamily.add("Edwar");
+console.log(myFamily);
+
+myFamily.add("JavaScript");
+console.log(myFamily);
